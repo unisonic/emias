9309fd9fe236bd70b67926514127dd3e55ab4fd3
unisonic <chimpoda@gmail.com>
2024-01-23 02:24:11 +0300

    Refactoring processing full state

--- src/process_fullstate.cpp
+++ src/process_fullstate.cpp
@@ -3,72 +3,123 @@
 
 #include <iostream>
 
-void NEmias::ProcessFullState() {
-    for (const auto& [chatId, entries] : NEmias::GFullState.items()) {
-        for (const auto& [requestId, entry] : entries.items()) {
-            std::string omsNumber = entry[std::to_string(OMS_ID)].template get<std::string>();
-            std::string birthDate = entry[std::to_string(BIRTHDATE)].template get<std::string>();
+namespace {
 
-            //  Check existing appointments
-            TJson requestAppointments = TJson::parse(R"({"id": "1", "jsonrpc": "2.0", "method": "getAppointmentReceptionsByPatient"})");
-            requestAppointments["params"]["omsNumber"] = omsNumber; 
-            requestAppointments["params"]["birthDate"] = birthDate;
-            TJson existingAppointments = NEmias::PostRequest(std::move(requestAppointments));
+    struct TRequestData {
+        std::string chatId;
+        std::string requestId;
 
-            TJson::number_unsigned_t appointmentId = 0;
-            TJson::number_unsigned_t specialityId = entry[std::to_string(SPECIALITY_ID)].template get<TJson::number_unsigned_t>();
-            for (const auto& appointment : existingAppointments["result"]) {
-                const TJson::number_unsigned_t appointmentSpecialityId = appointment["toDoctor"]["specialityId"].template get<TJson::number_unsigned_t>();
-                if (specialityId == appointmentSpecialityId || specialityId == 200 && appointmentSpecialityId == 602) {
-                    specialityId = appointmentSpecialityId;
-                    appointmentId = appointment["id"].template get<TJson::number_unsigned_t>();
-                }
+        std::string firstName;
+        std::string lastName;
+        std::string secondName;
+
+        std::string omsNumber;
+        std::string birthDate;
+
+        std::string startDate;
+        std::string finishDate;
+
+        TJson::number_unsigned_t specialityId;
+    };
+
+    TJson::number_unsigned_t GetExistingAppointment(const TRequestData& requestData) {
+        auto requestAppointments = TJson::parse(R"({"id": "1", "jsonrpc": "2.0", "method": "getAppointmentReceptionsByPatient"})");
+        requestAppointments["params"]["omsNumber"] = requestData.omsNumber; 
+        requestAppointments["params"]["birthDate"] = requestData.birthDate;
+        const auto existingAppointments = NEmias::PostRequest(std::move(requestAppointments));
+
+        TJson::number_unsigned_t appointmentId = 0;
+        for (const auto& appointment : existingAppointments["result"]) {
+            const auto appointmentSpecialityId = appointment["toDoctor"]["specialityId"].template get<TJson::number_unsigned_t>();
+            if (requestData.specialityId == appointmentSpecialityId || requestData.specialityId == 200 && (appointmentSpecialityId == 602 || appointmentSpecialityId == 69)) {
+                appointmentId = appointment["id"].template get<TJson::number_unsigned_t>();
             }
+        }
+        
+        return appointmentId;
+    }
 
-            //  Retrieve available appointments
-            TJson requestDoctorsInfo = TJson::parse(R"({"id": "1", "jsonrpc": "2.0", "method": "getDoctorsInfo"})");
-            requestDoctorsInfo["params"]["omsNumber"] = omsNumber; 
-            requestDoctorsInfo["params"]["birthDate"] = birthDate;
-            requestDoctorsInfo["params"]["specialityId"] = specialityId;
-            if (appointmentId != 0) {
-                requestDoctorsInfo["params"]["appointmentId"] = appointmentId;
+    bool InspectAvailableResource(
+            const TRequestData& requestData,
+            const TJson::number_unsigned_t appointmentId,
+            const TJson::number_unsigned_t availableResourceId)
+    {
+        TJson requestAvailableResource = TJson::parse(R"({"id": "1", "jsonrpc": "2.0", "method": "getAvailableResourceScheduleInfo"})");
+        requestAvailableResource["params"]["omsNumber"] = requestData.omsNumber;
+        requestAvailableResource["params"]["birthDate"] = requestData.birthDate;
+        requestAvailableResource["params"]["specialityId"] = requestData.specialityId;
+        requestAvailableResource["params"]["availableResourceId"] = availableResourceId; 
+        if (appointmentId != 0) {
+            requestAvailableResource["params"]["appointmentId"] = appointmentId;
+        }
+        TJson availableResource = NEmias::PostRequest(std::move(requestAvailableResource));
+
+        if (availableResource["result"].empty()) {
+            return false;
+        }
+        
+        bool appointmentFound = false;
+        for (const auto& slot : availableResource["result"]["scheduleOfDay"]) {
+            std::string slotDate = slot["date"].template get<std::string>();
+            if (requestData.startDate <= slotDate && slotDate <= requestData.finishDate) { 
+                NEmias::GMainBot.getApi().sendMessage(requestData.chatId, "По запросу <b>" + requestData.requestId + "</b> доступна запись на дату <b>" + slotDate + "</b>", false, 0, nullptr, "HTML");
+                appointmentFound = true;
             }
-            TJson doctorsInfo = NEmias::PostRequest(std::move(requestDoctorsInfo)); 
+        }
 
-            //  Find suitable appointment
-            std::string firstName = entry[std::to_string(FIRST_NAME)].template get<std::string>();
-            std::string lastName = entry[std::to_string(LAST_NAME)].template get<std::string>();
-            std::string secondName = entry[std::to_string(SECOND_NAME)].template get<std::string>();
-            for (const auto& item : doctorsInfo["result"]) {
-                if (item["mainDoctor"]["firstName"].template get<std::string>() != firstName
-                    || item["mainDoctor"]["lastName"].template get<std::string>() != lastName
-                    || item["mainDoctor"]["secondName"].template get<std::string>() != secondName
-                ) { continue; }
+        return appointmentFound;
+    }
 
-                TJson::number_unsigned_t availableResourceId = item["id"].template get<TJson::number_unsigned_t>();
-                TJson requestAvailableResource = TJson::parse(R"({"id": "1", "jsonrpc": "2.0", "method": "getAvailableResourceScheduleInfo"})");
-                requestAvailableResource["params"]["omsNumber"] = omsNumber;
-                requestAvailableResource["params"]["birthDate"] = birthDate;
-                requestAvailableResource["params"]["specialityId"] = specialityId;
-                requestAvailableResource["params"]["availableResourceId"] = availableResourceId; 
+    std::vector<std::pair<std::string, std::string>> ProcessRequests() {
+        std::vector<std::pair<std::string, std::string>> confirmedRequests;
+
+        for (const auto& [chatId, requests] : NEmias::GFullState.items()) {
+            for (const auto& [requestId, request] : requests.items()) {
+                const TRequestData requestData {
+                    chatId,
+                    requestId,
+                    request[std::to_string(NEmias::FIRST_NAME)].template get<std::string>(),
+                    request[std::to_string(NEmias::LAST_NAME)].template get<std::string>(),
+                    request[std::to_string(NEmias::SECOND_NAME)].template get<std::string>(),
+                    request[std::to_string(NEmias::OMS_ID)].template get<std::string>(),
+                    request[std::to_string(NEmias::BIRTHDATE)].template get<std::string>(),
+                    request[std::to_string(NEmias::START_DATE)].template get<std::string>(),
+                    request[std::to_string(NEmias::FINISH_DATE)].template get<std::string>(),
+                    request[std::to_string(NEmias::SPECIALITY_ID)].template get<TJson::number_unsigned_t>()
+                };
+
+                const auto appointmentId = GetExistingAppointment(requestData);
+                TJson requestDoctorsInfo = TJson::parse(R"({"id": "1", "jsonrpc": "2.0", "method": "getDoctorsInfo"})");
+                requestDoctorsInfo["params"]["omsNumber"] = requestData.omsNumber; 
+                requestDoctorsInfo["params"]["birthDate"] = requestData.birthDate;
+                requestDoctorsInfo["params"]["specialityId"] = requestData.specialityId;
                 if (appointmentId != 0) {
-                    requestAvailableResource["params"]["appointmentId"] = appointmentId;
+                    requestDoctorsInfo["params"]["appointmentId"] = appointmentId;
                 }
-                TJson availableResource = NEmias::PostRequest(std::move(requestAvailableResource));
+                TJson doctorsInfo = NEmias::PostRequest(std::move(requestDoctorsInfo)); 
 
-                if (availableResource["result"].empty()) {
-                    continue;
-                }
-                
-                std::string startDate = entry[std::to_string(START_DATE)].template get<std::string>();
-                std::string finishDate = entry[std::to_string(FINISH_DATE)].template get<std::string>();
-                for (const auto& slot : availableResource["result"]["scheduleOfDay"]) {
-                    std::string slotDate = slot["date"].template get<std::string>();
-                    if (startDate <= slotDate && slotDate <= finishDate) { 
-                        NEmias::GMainBot.getApi().sendMessage(chatId, "По запросу <b>" + requestId + "</b> доступна запись: <b>" + slotDate + "</b>", false, 0, nullptr, "HTML");
+                for (const auto& item : doctorsInfo["result"]) {
+                    if (item["mainDoctor"]["firstName"].template get<std::string>() != requestData.firstName
+                        || item["mainDoctor"]["lastName"].template get<std::string>() != requestData.lastName
+                        || item["mainDoctor"]["secondName"].template get<std::string>() != requestData.secondName) {
+                        continue;
+                    }
+
+                    if (InspectAvailableResource(requestData, appointmentId, item["id"].template get<TJson::number_unsigned_t>())) {
+                        confirmedRequests.emplace_back(chatId, requestId);
                     }
                 }
             }
         }
+
+        return confirmedRequests;
+    }
+
+}
+
+void NEmias::ProcessFullState() {
+    const auto confirmedRequests = ProcessRequests();
+    for (const auto& [chatId, requestId] : confirmedRequests) {
+        NEmias::GFullState[chatId].erase(requestId);
     }
 }
